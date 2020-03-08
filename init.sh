b6ce11191cb9caa56703463869fbdcdfc96bafc1
lokehoke <ibymif2000@yandex.ru>
2020-03-08 17:11:53 +0300

    change docker server

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 root=`pwd`
 
+
 echo "compile front"
 cd src/front/
 yarn install
 yarn webpack
 cd $root
 
-echo $root
 
 echo "start server"
 cd src/server/
-sudo docker build -t best_hack_server .
-sudo docker run -p 80:80  best_hack_server
+sudo docker build -t lokehoke/best_hack_server .
+sudo docker run -p 1337:3000 lokehoke/best_hack_server
 cd $root
