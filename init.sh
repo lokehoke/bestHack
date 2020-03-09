7584728b2591f73424baf557880aff91248d5f73
lokehoke <ibymif2000@yandex.ru>
2020-03-09 15:53:30 +0300

    postgres in docker

--- init.sh
+++ init.sh
@@ -7,9 +7,11 @@ yarn install
 yarn webpack
 cd $root
 
+
 echo "start server"
 cd src/server/
 yarn install
 cd $root
-sudo docker build -t lokehoke/best_hack_server -f src/server/Dockerfile .
-sudo docker run -p 3000:3000 lokehoke/best_hack_server
+
+sudo docker-compose build
+sudo docker-compose up
