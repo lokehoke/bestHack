9528332366af39c1b04fc695cc9f66bb0956f8a2
Igor Murashev <igmur00@mail.ru>
2020-03-10 21:50:52 +0300

    Init reg page

---
+++ src/front/js/bestHack/components/pages/auth-reg-page/reg-page/reg-block.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Descr from '../descr.jsx';
+import Reg from './reg.jsx';
+
+class RegBlock extends React.Component {
+    constructor(props){
+        super(props);
+    };
+
+    render() {
+        return (
+            <div className="col-5">
+                <div className="auth-block">
+                    <Descr />
+                    <Reg />
+                </div>
+            </div>
+            
+        );
+    };
+};
+
+export default RegBlock;
--- src/front/js/bestHack/components/pages/main.jsx
+++ src/front/js/bestHack/components/pages/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import AuthPage from './auth-reg-page/auth-page/auth-page.jsx';
-
+import RegPage from './auth-reg-page/reg-page/reg-page.jsx';
 
 class Main extends React.Component {
     constructor(props){
