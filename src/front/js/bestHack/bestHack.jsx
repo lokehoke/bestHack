e5b880babdd398c2e59f9ad661eb3a839822d750
lokehoke <ibymif2000@yandex.ru>
2020-03-13 04:03:01 +0300

    add context menu

--- src/front/js/bestHack/bestHack.jsx
+++ src/front/js/bestHack/bestHack.jsx
@@ -37,11 +37,6 @@ class BestHack {
             e.preventDefault();
             return false;
         });
-
-        document.addEventListener('contextmenu', e => {
-            e.preventDefault();
-            return false;
-        });
     };
 }
 
