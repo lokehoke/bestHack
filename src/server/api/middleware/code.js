ebf7e25f33da081b6e0b2563d9d2de6011fb3d56
sergeyampo <dev@sergeyampo.ru>
2020-03-13 11:35:49 +0900

    Merge branch 'master' of https://github.com/lokehoke/bestHack
    # Conflicts:
    #	public/resources/resources/js/bundle.js

--- src/server/api/middleware/code.js
+++ src/server/api/middleware/code.js
@@ -40,7 +40,6 @@ class CodeMiddleware{
     //if the code was successfully compiled -> next
     //else call error handling mw
     static isCodeCompiled(req, res, next){
-//        if(req.compiled.is) { //RETURN DONT COMMIT
         if(req.compiled.is) {
             return next();
         }
